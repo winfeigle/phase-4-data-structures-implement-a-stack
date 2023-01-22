ae9e69ba7870d10592cae2e2814836e179b1a647
Win Feigle <win.feigle@gmail.com>
2023-01-22 09:33:48 -0700

    commit tree 0c0c6d32e7f6a56676521d07aa29d7def3ca0704

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,46 @@
 # your code here
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(val)
+        raise "Stack Overflow" if full?
+
+        @stack.push(val)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        # making sure limit exists, first.. then checking size
+        @limit && @stack.size == @limit
+    end
+
+    def search(val)
+        @stack.each_with_index do |item, index|
+            return size - index - 1 if val == item
+        end
+
+        # This is the else...
+        -1
+    end
+
+
+end
